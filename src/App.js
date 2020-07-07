a2f0a2d4ae5bc54e41d2fa6bf0e9843aca8c293d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-07 14:22:28 +0100

    Adds custom banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Main from './Main';
+import Guardian from './guardian.png'
 
 import './App.css';
 
@@ -8,6 +9,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+      <img src={Guardian}></img>
       <NavBar />
       <Main />
     </div>
