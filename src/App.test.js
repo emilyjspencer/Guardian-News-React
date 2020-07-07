9527bd9b12c540e562878acbd4661baf623a28db
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-07 16:10:03 +0100

    Adds passing test for rending the Main component in the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
+  it ('renders an instance of the Main component', () => {
+    expect(wrapper.find('Main').length).toEqual(1);
+  })
 });
