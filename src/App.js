94cd41f5977ab27c9e20579b2c7cbf7a17ba53ad
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-07 10:29:59 +0100

    News stories with publication data showing on page. Currently, if the API key is replaced with the key variable, the data does not display. For the purpose of pushing ot GitHub, have replaced it with this key variable.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import axios from 'axios';
 
 const App = ()  => {
   console.log(process.env.REACT_APP_API_KEY)
+  
 
   const [stories, setStories ] = useState([]);
 
-
+  useEffect(() => {
+    const key = process.env.REACT_APP_API_KEY
+    axios.get(`https://content.guardianapis.com/search?api-key=key`)
+      .then(result => {
+        console.log(result)
+        const newStories = result.data.response.results
+        
+         console.log(newStories[0])
+           setStories(newStories);
+         });
+        }, []);
+      
+      
   return (
     <div>
       <h1>Guardian News</h1>
       <ul>
         {stories.map(story => (
-          <li> key={story.id}>{story.title}</li>
+        <li key={story.id}>
+          {story.webUrl}
+          <br />
+          <br />
+          {story.webPublicationDate}
+        </li>
+      
         ))}
-      </ul>
+        </ul>
     </div>
+    
   );
-}
-
+        }
+  
+        
 export default App;
