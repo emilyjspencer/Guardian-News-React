3663a8170f2d142536ad54d6310a2fb14a141d9e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-07 02:09:08 +0100

    Adds useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import axios from 'axios';
+
+const App = ()  => {
+
+  const [stories, setStories ] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Guardian News</h1>
+      <ul>
+        {stories.map(story => (
+          <li> key={story.id}>{story.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
