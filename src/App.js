c01edc5b856dc11bace5d668a9d85a5e8011f90d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-07 14:04:57 +0100

    Adds NavBar and Main components. Makes new story links clickable. Again - API key has been removed. Functionality works.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import NavBar from './NavBar';
+import Main from './Main';
 
 import './App.css';
-import axios from 'axios';
 
-const App = ()  => {
-  console.log(process.env.REACT_APP_API_KEY)
-  
 
-  const [stories, setStories ] = useState([]);
-
-  useEffect(() => {
-    const key = process.env.REACT_APP_API_KEY
-    axios.get(`https://content.guardianapis.com/search?api-key=key`)
-      .then(result => {
-        console.log(result)
-        const newStories = result.data.response.results
-        
-         console.log(newStories[0])
-           setStories(newStories);
-         });
-        }, []);
-      
-      
+const App = () => {
   return (
-    <div>
-      <h1>Guardian News</h1>
-      <ul>
-        {stories.map(story => (
-        <li key={story.id}>
-          {story.webUrl}
-          <br />
-          <br />
-          {story.webPublicationDate}
-        </li>
-      
-        ))}
-        </ul>
+    <div className="App">
+      <NavBar />
+      <Main />
     </div>
-    
-  );
-        }
-  
-        
+  )
+}
+
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+import './App.css';
+
+
+const Main = ()  => {
+  console.log(process.env.REACT_APP_API_KEY)
+  
+
+  const [stories, setStories ] = useState([]);
+
+  useEffect(() => {
+   const key = process.env.REACT_APP_API_KEY;
+    axios.get(`https://content.guardianapis.com/search?api-key=key`)
+      .then(result => {
+        console.log(result)
+        const newStories = result.data.response.results
+        
+         console.log(newStories[0])
+           setStories(newStories);
+         });
+        }, []);
+      
+      
+  return (
+    <div className="App">
+        
+      <br />
+      <h1>Guardian News</h1>
+      <ul>
+        {stories.map(story => (
+        <li key={story.id}>
+          <a href={story.webUrl}>{story.webUrl}</a>
+          <br />
+          <br />
+          {story.webPublicationDate}
+        </li>
+      
+        ))}
+        </ul>
+   
+      </div>
+
+    
+  );
+        }
+  
+        
+export default Main;
