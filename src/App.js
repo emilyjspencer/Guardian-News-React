81b5f3294982ba658cf2022ce4dfcdb4d38f9279
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-31 19:00:02 +0000

    Changes background colour for bottom section of page

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     </div>
       <Main />
       <div className="strip2">
-      <img className="london2" src={banner3} />
+      <div className="london2" />
       </div>
     </div>
   )
